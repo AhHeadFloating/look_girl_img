9f9f6209ca063f5833bf501dbc28082192924dea
头上的阿飘 <34568119+AhHeadFloating@users.noreply.github.com>
2019-05-07 16:16:19 +0800

    “”

---
+++ lib/ui/SimpleList.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:show_girl_img/entity/Poster.dart';
+
+class SimpleList extends StatefulWidget{
+
+  List<Poster> items;
+
+  SimpleList({Key key,@required this.items}):super(key:key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _SimpleListState();
+  }
+
+}
+
+class _SimpleListState extends State<SimpleList>{
+  
+  @override
+  Widget build(BuildContext context) {
+    if(widget.items == null){
+      widget.items = new List<Poster>();
+    }
+    return ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (context,index){
+          return Image.network(widget.items[index].url);
+        }
+    );
+    
+  }
+  
+}
